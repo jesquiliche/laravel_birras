a1252943894610e618f1b0a32f8c4959cf835844
Jesús Quintana <jesquiliche@hotmail.comm>
2023-12-31 21:49:14 +0100

    Añadiendo System

--- app/Http/Controllers/Api/V1/SystemController.php
+++ app/Http/Controllers/Api/V1/SystemController.php
@@ -126,7 +126,7 @@ class SystemController extends Controller
             data_length / (1024 * 1024) AS data_size_mb,
             index_length / (1024 * 1024) AS index_size_mb
             FROM information_schema.tables
-            WHERE table_schema = 'laravel_birras';
+            WHERE table_schema = '{$databaseName}';
         ");
 
         return response()->json($resultados);
