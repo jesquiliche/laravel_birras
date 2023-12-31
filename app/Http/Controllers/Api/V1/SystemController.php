c4e8a04a053b60022271f39478295c94289a1584
Jesús Quintana <jesquiliche@hotmail.comm>
2023-12-31 22:05:30 +0100

    Añadiendo System

--- app/Http/Controllers/Api/V1/SystemController.php
+++ app/Http/Controllers/Api/V1/SystemController.php
@@ -126,7 +126,8 @@ class SystemController extends Controller
             data_length / (1024 * 1024) AS data_size_mb,
             index_length / (1024 * 1024) AS index_size_mb
             FROM information_schema.tables
-            WHERE table_schema = '{$databaseName}';
+            WHERE table_schema = '{$databaseName}'
+            AND table_type = 'BASE TABLE'; -- Solo tablas, no vistas ni tablas de sistema;
         ");
 
         return response()->json($resultados);
