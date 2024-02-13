3e9f918b763d2700342eca1b19baa1a901c36ba7
Jesús Quintana <jesquiliche@hotmail.comm>
2024-02-13 11:51:11 +0100

    Añadiendo poblaciones y provincias

--- app/Http/Controllers/Api/V1/PoblacionController.php
+++ app/Http/Controllers/Api/V1/PoblacionController.php
@@ -13,24 +13,31 @@ class PoblacionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-      /**
-         * @OA\Get(
-         *     path="/api/v1/poblaciones",
-         *     operationId="index",
-         *     tags={"Poblaciones"},
-         *     summary="Obtener todas las poblaciones",
-         *     description="Devuelve todas las poblaciones ordenadas por nombre.",
-         *     @OA\Response(
-         *         response=200,
-         *         description="Devuelve todas las poblaciones ordenadas por nombre.",
-         *     )
-         * )
-         */
-      
-    public function index()
-    {
-        return Poblacion::orderBy('nombre')->get();
-    }
+    /**
+     * @OA\Get(
+     *     path="/api/v1/poblaciones",
+     *     operationId="index",
+     *     tags={"Poblaciones"},
+     *     summary="Obtener todas las poblaciones",
+     *     description="Devuelve todas las poblaciones ordenadas por nombre o filtradas por código si se proporciona.",
+     *     @OA\Parameter(
+     *         name="codigo",
+     *         in="query",
+     *         description="Código de la población a filtrar.",
+     *         required=false,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Devuelve todas las poblaciones ordenadas por nombre o filtradas por código si se proporciona.",
+     *     )
+     * )
+     */
 
+    public function index(Request $request)
+    {
+        $codigo = $request->input('codigo', ''); // Valor predeterminado es una cadena vacía
+
+        return Poblacion::where('codigo', $codigo)->orderBy('nombre')->get();
+    }
 }
-    
