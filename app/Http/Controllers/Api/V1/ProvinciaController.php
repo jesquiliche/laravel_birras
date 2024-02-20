b4b554d62f0017f332ce357aa85c3c36ef430aa8
Jesús Quintana <jesquiliche@hotmail.comm>
2024-02-20 14:31:56 +0100

    Añadiendo endpoints

--- app/Http/Controllers/Api/V1/ProvinciaController.php
+++ app/Http/Controllers/Api/V1/ProvinciaController.php
@@ -14,11 +14,6 @@ class ProvinciaController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    /*public function __construct()
-    {
-        $this->middleware('auth:api');
-    }*/
-
     /**
      * @OA\Get(
      *      path="/api/v1/provincias",
@@ -48,10 +43,48 @@ class ProvinciaController extends Controller
         return Provincia::orderBy('nombre')->get();
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/v1/provincias/{codigo}",
+     *      operationId="getProvinciaByCodigo",
+     *      tags={"Provincias"},
+     *      summary="Obtener una provincia por su código",
+     *      description="Recupera una provincia de la base de datos por su código y la devuelve como una respuesta JSON.",
+     *      @OA\Parameter(
+     *          name="codigo",
+     *          in="path",
+     *          required=true,
+     *          description="Código de la provincia (varchar de longitud 5)",
+     *          @OA\Schema(
+     *              type="string",
+     *              maxLength=5
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Provincia encontrada",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="integer", example=1),
+     *              @OA\Property(property="nombre", type="string", example="Provincia A"),
+     *              @OA\Property(property="created_at", type="string", format="date-time"),
+     *              @OA\Property(property="updated_at", type="string", format="date-time"),
+     *          ),
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Provincia no encontrada",
+     *      )
+     * )
+     */
     public function show(string $codigo)
     {
-        // Recupera todas las provincias desde la base de datos y las devuelve como una respuesta JSON ordenadas por nombre
-        return Provincia::where('codigo','=',$codigo)->get();
+        // Recupera una provincia desde la base de datos por su código y la devuelve como una respuesta JSON
+        $provincia = Provincia::where('codigo', '=', $codigo)->first();
+        
+        if (!$provincia) {
+            return response()->json(['error' => 'Provincia no encontrada'], 404);
+        }
+        
+        return $provincia;
     }
-    
 }
