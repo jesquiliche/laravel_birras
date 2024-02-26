5f24d060f5ce16094f85301e408051d7fea30c1d
Jesús Quintana <jesquiliche@hotmail.comm>
2024-02-26 20:08:12 +0100

    Actualizando documentación

--- app/Http/Controllers/OrdenController.php
+++ app/Http/Controllers/OrdenController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Orden;
 use App\Models\Cerveza;
 use App\Models\Detalle;
+use App\Models\OrdenDireccion;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OrdenController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->only(['store', 'destroy', 'update']);
+    }
     /**
      * Display a listing of the resource.
      */
@@ -30,6 +36,65 @@ class OrdenController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    /**
+     * @OA\Post(
+     *     path="/api/v1/ordenes",
+     *     summary="Crear una nueva orden",
+     *     tags={"Ordenes"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"address", "articulos"},
+     *             @OA\Property(property="address", type="object", description="Datos de dirección",
+     *                 @OA\Property(property="nombre", type="string", example="Jesús"),
+     *                 @OA\Property(property="apellidos", type="string", example="Quintana Esquiliche"),
+     *                 @OA\Property(property="calle", type="string", example="C/ América"),
+     *                 @OA\Property(property="numero", type="string", example="34"),
+     *                 @OA\Property(property="escalera", type="string", example=null),
+     *                 @OA\Property(property="piso", type="string", example="1"),
+     *                 @OA\Property(property="puerta", type="string", example="4"),
+     *                 @OA\Property(property="poblacion", type="string", example="08007"),
+     *                 @OA\Property(property="provincia", type="string", example="08"),
+     *                 @OA\Property(property="user_id", type="integer", example="1"),
+     *                 @OA\Property(property="telefono", type="string", example="632816055")
+     *             ),
+     *             @OA\Property(property="articulos", type="array", description="Lista de artículos",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="id", type="integer", example="15"),
+     *                     @OA\Property(property="cantidad", type="integer", example="10")
+     *                 ),
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="id", type="integer", example="23"),
+     *                     @OA\Property(property="cantidad", type="integer", example="4")
+     *                 ),
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="id", type="integer", example="19"),
+     *                     @OA\Property(property="cantidad", type="integer", example="1")
+     *                 )
+     *             )
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Orden creada exitosamente",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Orden creada exitosamente")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error al procesar la orden",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string", example="Error al procesar la orden: mensaje de error")
+     *         )
+     *     )
+     * )
+     */
+
     public function store(Request $request)
     {
         try {
@@ -54,9 +119,9 @@ class OrdenController extends Controller
             }
 
             // Calcular el subtotal y el IVA
-            $total=round($total,2);
-            $subtotal = round($total / 1.21,2);
-            $iva = round($total - $subtotal,2);
+            $total = round($total, 2);
+            $subtotal = round($total / 1.21, 2);
+            $iva = round($total - $subtotal, 2);
 
             // Crear una nueva instancia de Orden y establecer los valores
             $orden = new Orden();
@@ -67,28 +132,36 @@ class OrdenController extends Controller
 
             // Guardar la orden en la base de datos
             $orden->save();
-            $ordenId=$orden->id;
+            $ordenId = $orden->id;
 
             foreach ($articulos as $articulo) {
-                $articuloD=new Detalle();
-                
-                $articuloD['orden_id']=$ordenId;
+                $articuloD = new Detalle();
+
+                $articuloD['orden_id'] = $ordenId;
                 // Buscar la cerveza por su id
-                $articuloId=$articulo['id'];
+                $articuloId = $articulo['id'];
                 $cerveza = Cerveza::find($articuloId);
-                print($articulo['cantidad']." ".$cerveza->stock."\n");
-                if($articulo['cantidad']>$cerveza->stock){
+
+                if ($articulo['cantidad'] > $cerveza->stock) {
                     throw new Exception('La cantidad de stock es insuficiente para el artículo: ' . $cerveza->nombre);
                 } else {
-                    $cerveza->stock=$cerveza->stock-$articulo['cantidad'];
+                    $cerveza->stock = $cerveza->stock - $articulo['cantidad'];
                     $cerveza->update();
                 }
-                $articuloD->cantidad=$articulo['cantidad'];
-                $articuloD->precio=$cerveza->precio;
-                $articuloD->cerveza_id=$articuloId;
+                $articuloD->cantidad = $articulo['cantidad'];
+                $articuloD->precio = $cerveza->precio;
+                $articuloD->cerveza_id = $articuloId;
                 $articuloD->save();
             }
 
+            // Obtener los datos de dirección del cuerpo de la solicitud
+            $addressData = $request->input('address');
+
+            // Asignar el ID de la orden al addressData
+            $addressData['orden_id'] = $orden->id;
+
+            // Guardar la dirección en la base de datos utilizando el método create
+            OrdenDireccion::create($addressData);
             DB::commit();
             return response()->json(['message' => 'Orden creada exitosamente'], 201);
         } catch (Exception $e) {
